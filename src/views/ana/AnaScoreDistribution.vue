5bfa69fba7750084660ac7f9f61a930e40366618
writer <2844194920@qq.com>
2020-05-04 18:27:11 +0800

    bug

--- src/views/ana/AnaScoreDistribution.vue
+++ src/views/ana/AnaScoreDistribution.vue
@@ -42,7 +42,7 @@
                             label="出卷人">
                     </el-table-column>
                 </el-table>
-                <el-dialog title="整体成绩分析" :visible.sync="column_value">
+                <el-dialog title="整体成绩分析" :visible.sync="column_value" :destroy-on-close="true">
                     <div id="column" :style="{width: '600px', height: '300px'}" ></div>
                     <div id="line_chart" :style="{width: '600px', height: '300px'}" ></div>
                 </el-dialog>
@@ -67,11 +67,53 @@
     </div>
 </template>
 
-<script>
+<script >
     export default {
         name: "score_distribution",
         data(){
             return{
+                //柱状图数据
+                option : {
+                    color: ['#3398DB'],
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: [
+                        {
+                            type: 'category',
+                            data: ["60以下", "60-69", "70-79", "80-89", "90-100"],
+                            axisTick: {
+                                alignWithLabel: true
+                            }
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ],
+                    series: [
+                        {
+                            name: '直接访问',
+                            type: 'bar',
+                            barWidth: '60%',
+                            data: [10, 52, 200, 334, 390]
+                        }
+                    ]
+                },
+                option_series_data:[10, 52, 200, 334, 390],
+                //柱状图div和折线图div
+                myChart:null,
+                line_chart:null,
                 //折线图数据
                 line_chart_option : {
                     xAxis: {
@@ -117,44 +159,7 @@
                 search_data:'',      //搜索输入框的数据
                 papers:['15级JAVA期末测试','16级JAVA期末测试','17级JAVA期末测试','18级JAVA期末测试'],          //试卷
                 activeNames: ['1'],
-                //柱状图数据
-                option : {
-                    color: ['#3398DB'],
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-                        }
-                    },
-                    grid: {
-                        left: '3%',
-                        right: '4%',
-                        bottom: '3%',
-                        containLabel: true
-                    },
-                    xAxis: [
-                        {
-                            type: 'category',
-                            data: ['60以下', '60-69', '70-79', '80-89', '90-100'],
-                            axisTick: {
-                                alignWithLabel: true
-                            }
-                        }
-                    ],
-                    yAxis: [
-                        {
-                            type: 'value'
-                        }
-                    ],
-                    series: [
-                        {
-                            name: '直接访问',
-                            type: 'bar',
-                            barWidth: '60%',
-                            data: [10, 52, 200, 334, 390]
-                        }
-                    ]
-                }
+
 
             }
         },
@@ -183,35 +188,75 @@
             },
             //列表点击事件
             list_click(row){
+
                 //true表示显示弹出框
                 this.column_value=true
-                //向后台发送请求获取数据
-                console.log(row.id)
+                let that = this
+                //向后台发送卷子id，获取该卷子分数区间和每个区间的人数
                 this.postRequest('http://localhost:8080/analysis/getTestPaperScoreById?id='+row.id).then(res=>{
                     if(res){
-                        console.log(res)
+                        let arr = new Array(res.length)
+                        for(let i=0;i<res.length;i++){
+                            arr[i] = res.section[i]
+                        }
+                        let people_num = new Array(res.people_num)
+                        for(let i=0;i<res.length;i++){
+                            people_num[i] = res.people_num[i]
+                        }
+
+
+                            this.$set(that.option.xAxis,'data',arr)
+                            this.$set(that.option.series,'data',people_num)
+
+
+                        // that.option.xAxis.data = arr
+                        // that.option.series.data = people_num
+
+                        //this.option_series_data = people_num
+
+
+                        // that.line_chart_option.xAxis[0].data = arr
+                        // that.line_chart_option.series[0].data = res.people_num
+                        // that.$set(that.option.xAxis,'data',arr)
+                        // that.$set(that.option.series,'data',res.people_num)
+                        // that.$forceUpdate()
+
                     }
                 })
 
                 //300毫秒后执行画图函数
                 // console.log(row.id)
-                setTimeout(() => {
-                    this.drawLine()
-                },300)
+                // setTimeout(() => {
+                //     this.drawLine()
+                // },100)
             },
             //画柱状图和折线图的函数
             drawLine(){
-                let myChart = this.$echarts.init(document.getElementById("column"))
-                let line_chart = this.$echarts.init(document.getElementById("line_chart"))
-                line_chart.setOption(this.line_chart_option)
-                myChart.setOption(this.option)
+                // document.getElementById("column").removeAttribute("_echarts_instance_");
+                this.myChart = this.$echarts.init(document.getElementById("column"))
+                this.line_chart = this.$echarts.init(document.getElementById("line_chart"))
+                this.$forceUpdate();
+                this.myChart.clear()
+                console.log(this.option.xAxis.data)
+                console.log(this.option.series.data)
+                //this.line_chart.setOption(this.line_chart,true)
+                this.myChart.setOption(this.option,true)
+
+
+            }
+        },
+        watch:{
+            option:{
+                handler(newVal,oldVal){
+                    console.log("heihehiwheihiwhehiwheiwhie")
+                    this.drawLine()
+                },deep:true
             }
         },
         mounted() {
             let that = this
             //挂载结束后立即向后台请求试卷列表的数据，然后将试卷列表显示在表格中
             this.$http.get('http://localhost:8080/analysis/getListOfTestPaper').then(function (res) {
-                console.log(res)
                 that.count_TestPaper = res.length       //将返回数据总数保存起来
                 //将返回来的数组放到表格里
                 that.list_oftestpaper = res
